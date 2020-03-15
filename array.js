5292dbd4a7c2d23551d89c3368b646c7c2a1183d
Rectmoon <1426890675@qq.com>
2020-03-16 06:23:22 +0800

    ~

--- array.js
+++ array.js
@@ -12,7 +12,11 @@ const res3 = JSON.stringify(arr)
   .split(',')
   .map(n => ~~n)
 
-const reduceFlat = arr => arr.reduce((res, next) => res.concat(Array.isArray(next) ? reduceFlat(next) : next), [])
+const reduceFlat = arr =>
+  arr.reduce(
+    (res, next) => res.concat(Array.isArray(next) ? reduceFlat(next) : next),
+    []
+  )
 const res4 = reduceFlat(arr)
 
 console.log(res1)
@@ -20,7 +24,20 @@ console.log(res2)
 console.log(res3)
 console.log(res4)
 
-const A = ['D1', 'D2', 'D3', 'A1', 'A2', 'A3', 'C1', 'C2', 'C3', 'B1', 'B2', 'B3']
+const A = [
+  'D1',
+  'D2',
+  'D3',
+  'A1',
+  'A2',
+  'A3',
+  'C1',
+  'C2',
+  'C3',
+  'B1',
+  'B2',
+  'B3'
+]
 const B = ['B', 'C', 'D', 'A']
 
 let n = 0
@@ -34,7 +51,7 @@ for (let i = 0, len = B.length; i < len; i++) {
 }
 console.log(A)
 
-!(function() {
+!(function () {
   const result = {
     1: 100,
     3: 300,
@@ -45,8 +62,8 @@ console.log(A)
   console.log(res)
 })()
 
-!(function() {
-  Array.prototype.rotate = function(k) {
+!(function () {
+  Array.prototype.rotate = function (k) {
     if (k < 0 || k === 0 || k === this.length) return this
     if (k > this.length) k = k % this.length
     let i = 0
@@ -61,3 +78,21 @@ console.log(A)
   const arr = [1, 2, 3, 4, 5, 6, 7]
   console.log(arr.rotate(2))
 })()
+
+// 滑动窗口最大值
+function maxInWindows (a, size) {
+  const arr = []
+  if (a.length == 0 || size <= 0) return arr
+  if (size === 1) return a
+
+  const len = a.length - size + 1
+  let max
+  for (let i = 0; i < len; i++) {
+    max = Math.max.apply(null, a.slice(i, i + size))
+    arr[i] = max
+  }
+  return arr
+}
+
+const a = [2, 3, 4, 2, 6, 2, 5, 1]
+console.log(maxInWindows(a, 3))
