903a22099d4abd9fc378240450dcae4c5625b873
zhulingying <zhulingying@co-mall.com>
2020-12-21 17:00:11 +0800

    ~

--- curry.js
+++ curry.js
@@ -1,4 +1,4 @@
-function add1(a, b) {
+function add1 (a, b) {
   if (arguments.length == 1) return x => a + x
   return a + b
 }
@@ -6,9 +6,9 @@ function add1(a, b) {
 console.log(add1(2, 3))
 console.log(add1(2)(3))
 
-function add2(x) {
+function add2 (x) {
   var res = x
-  var tmp = function(y) {
+  var tmp = function (y) {
     res += y
     return tmp
   }
@@ -23,15 +23,15 @@ console.log(add2(1)(2)(3)(4).toString())
 console.log(add2(1, 2, 3, 4, 5).toString())
 console.log(add2(1, 2, 3, 4)(5).toString())
 
-function add3() {
+function add3 () {
   var ap = Array.prototype
   var _args = ap.slice.call(arguments)
-  var tmp = function() {
-    var inner = function() {
+  var tmp = function () {
+    var inner = function () {
       _args = _args.concat(ap.slice.call(arguments))
       return inner
     }
-    inner.toString = function() {
+    inner.toString = function () {
       return _args.reduce((a, b) => a + b)
     }
     return inner
@@ -42,3 +42,26 @@ function add3() {
 console.log(add3(1, 2, 3, 4, 5).toString())
 console.log(add3(1, 2, 3, 4)(5).toString())
 console.log(add3(1)(2)(3)(4)(5).toString())
+
+function sum (a, b, c) {
+  return a + b + c
+}
+
+function curry (fn) {
+  const argsList = [...arguments].splice(1)
+
+  return function () {
+    const newArgsList = argsList.concat([...arguments])
+    if (newArgsList.length < fn.length) {
+      // 如果接收的参数还没有到达函数参数的个数继续收集参数
+      return curry.apply(this, [fn, ...newArgsList])
+    } else {
+      return fn.apply(this, newArgsList)
+    }
+  }
+}
+const sumAll = curry(sum)
+
+console.log(sum.length)
+console.log(sumAll(1)(2)(3)) // 6
+console.log(sumAll(1)(2, 3)) // 6
